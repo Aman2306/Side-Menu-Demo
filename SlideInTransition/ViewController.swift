78afcff31c78275c3374aa2a6b7a34d9453bd9eb
Aman Meena <mybirthday2306@gmail.com>
2019-03-05 21:38:01 +0530

    Initial Commit

---
+++ SlideInTransition/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SlideInTransition
+//
+//  Created by Aman Meena on 05/03/19.
+//  Copyright © 2019 Aman Meena. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
